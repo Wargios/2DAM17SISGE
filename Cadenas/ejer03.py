d77e5b9443b46a383a95ccb9a5196afecdb089d2
fran <fran@profesor-desktop>
2016-12-15 14:32:47 +0100

    Subida

---
+++ Cadenas/ejer03.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8-*-
+''' 
+3. (Samuel)Crear un programa que lea por teclado una cadena y un carácter, 
+y reemplace todos los espacios por el carácter. Ej: mi archivo de texto.txt 
+y _ debería devolver mi_archivo_de_texto.txt
+'''
